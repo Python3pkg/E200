aa260647ba0d509f45f90139a221c7d82d3b1db3
Joel Frederico <joelfred@slac.stanford.edu>
2015-05-09 08:51:56 -0700

    More updates

--- E200_load_data.py
+++ E200_load_data.py
@@ -21,77 +21,72 @@ logger = logging.getLogger(__name__)
 __all__ = ['E200_load_data']
 
 
-def E200_load_data(filename, writefile = None, verbose = False, readonly = False, local = False):
+def E200_load_data(filename, savefile=None, verbose = False, local = False):
     logger.log(level = loggerlevel, msg = 'Input is: filename={}'.format(filename))
-
-    # ======================================
-    # Get a verified filename
-    # ======================================
-    if local:
-        vfn = Filename(path='temp.mat', local=local)
-    else:
-        vfn = Filename(path=filename, local=local)
-    processed_path = vfn.processed_path
-
-    logger.log(level=loggerlevel, msg='Processed file path is: processed_file_path={}'.format(processed_path))
 
     # ======================================
     # Create temporary directory
     # ======================================
-    with tempfile.TemporaryDirectory() as tempdir:
-        tempfilename = 'temp.h5'
-        temppath = os.path.join(tempdir,tempfilename)
+    if savefile is None:
+        with tempfile.TemporaryDirectory() as tempdir:
+            tempfilename = 'temp.h5'
+            temppath = os.path.join(tempdir, tempfilename)
+            return _process_file(filename=filename, temppath=temppath)
+    else:
+        return _process_file(filename=filename, temppath=savefile)
 
-        # ======================================
-        # Have matlab process file
-        # ======================================
-        logger.log(level=loggerlevel, msg='Processed file not found, calling matlab to process file.')
-        pwd = os.getcwd()
-        # matlab = '/Applications/MATLAB_R2014b.app/bin/matlab -nodisplay -nosplash'
-        matlab = 'fmatlab -nodisplay -nosplash'
-        command = '{matlab} -r "addpath(fullfile(getenv(\'HOME\'),\'E200_DRT/E200_data/\'),\'~/python-dev-modules/E200/\');cd(\'{pwd}\');convert_mat_file(\'{filename}\',\'{outfile}\');exit;"'.format(matlab=matlab, pwd=pwd, filename=filename, outfile=temppath)
-        
-        logger.log(level=loggerlevel, msg='Command given is: {}'.format(command))
-        subprocess.call(shlex.split(command))
 
-        # ======================================
-        # Load file processed by matlab
-        # ======================================
-        logger.log(level=loggerlevel, msg='Loading processed file')
-        f = _h5.File(temppath, 'r', driver='core', backing_store=False)
+def _process_file(filename, temppath):
+    # ======================================
+    # Have matlab process file
+    # ======================================
+    logger.log(level=loggerlevel, msg='Processed file not found, calling matlab to process file.')
+    pwd = os.getcwd()
+    # matlab = '/Applications/MATLAB_R2014b.app/bin/matlab -nodisplay -nosplash'
+    matlab = 'fmatlab -nodisplay -nosplash'
+    command = '{matlab} -r "addpath(fullfile(getenv(\'HOME\'),\'E200_DRT/E200_data/\'),\'~/python-dev-modules/E200/\');cd(\'{pwd}\');convert_mat_file(\'{filename}\',\'{outfile}\');exit;"'.format(matlab=matlab, pwd=pwd, filename=filename, outfile=temppath)
+    
+    logger.log(level=loggerlevel, msg='Command given is: {}'.format(command))
+    subprocess.call(shlex.split(command))
 
-        output = Data(read_file = f)
+    # ======================================
+    # Load file processed by matlab
+    # ======================================
+    logger.log(level=loggerlevel, msg='Loading processed file')
+    f = _h5.File(temppath, 'r', driver='core', backing_store=False)
 
-        return output
-        # loadreq = E200_dataset2str(output.rdrill.data.VersionInfo.loadrequest)
-        # logger.log(level=loggerlevel, msg='Load request: {}'.format(loadreq))
+    output = Data(read_file = f)
 
-        # # ======================================
-        # # Determine if the loaded file matches
-        # # the requested file
-        # # ======================================
-        # if loadreq == filename:
-        #     logger.log(level=loggerlevel, msg='Request matches loaded file, continuing...')
-        #     return output
-        # else:
-        #     logger.log(level=loggerlevel, msg='Request doesn''t match loaded file, removing processed file and retrying...')
-        #     try:
-        #         os.remove(vfn.processed_path)
-        #     except OSError:
-        #         pass
+    return output
+    # loadreq = E200_dataset2str(output.rdrill.data.VersionInfo.loadrequest)
+    # logger.log(level=loggerlevel, msg='Load request: {}'.format(loadreq))
 
-        #     try:
-        #         os.remove(vfn.py_processed_path)
-        #     except OSError:
-        #         pass
-        #     ipdb.set_trace()
-        #     return E200_load_data(
-        #         filename   = filename,
-        #         writefile  = writefile,
-        #         verbose    = verbose,
-        #         readonly   = readonly,
-        #         local      = local
-        #         )
+    # # ======================================
+    # # Determine if the loaded file matches
+    # # the requested file
+    # # ======================================
+    # if loadreq == filename:
+    #     logger.log(level=loggerlevel, msg='Request matches loaded file, continuing...')
+    #     return output
+    # else:
+    #     logger.log(level=loggerlevel, msg='Request doesn''t match loaded file, removing processed file and retrying...')
+    #     try:
+    #         os.remove(vfn.processed_path)
+    #     except OSError:
+    #         pass
+
+    #     try:
+    #         os.remove(vfn.py_processed_path)
+    #     except OSError:
+    #         pass
+    #     ipdb.set_trace()
+    #     return E200_load_data(
+    #         filename   = filename,
+    #         writefile  = writefile,
+    #         verbose    = verbose,
+    #         readonly   = readonly,
+    #         local      = local
+    #         )
 
 
 def _load_data(wf):
