50641815a69f1a21b24fae409c4f14f06d99bb74
Joel Frederico <joelfred@slac.stanford.edu>
2015-07-08 12:46:52 -0700

    Formatting

--- E200/E200_create_data.py
+++ E200/E200_create_data.py
@@ -7,23 +7,25 @@ logger = logging.getLogger(__name__)
 loggerlevel = 9
 
 __all__ = ['E200_create_data']
-def E200_create_data(group_parent,group_name):
-	# ======================================
-	# Check if group name already exists
-	# ======================================
-	if group_name in group_parent.keys():
-		logger.log(level=loggerlevel,msg='Data with name "{}" already exists.'.format(group_name))
-		return group_parent[group_name]
 
-	# ======================================
-	# Create different kinds of groups
-	# ======================================
-	group_child = group_parent.create_group(group_name)
-	group_child.create_dataset('UID',shape=(0,),maxshape=(None,),dtype=_np.int64)
 
-	ref_dtype = h5.special_dtype(ref=h5.Reference)
-	group_child.create_dataset('dat',shape=(0,),maxshape=(None,),dtype=ref_dtype)
+def E200_create_data(group_parent, group_name):
+    # ======================================
+    # Check if group name already exists
+    # ======================================
+    if group_name in group_parent.keys():
+        logger.log(level=loggerlevel, msg='Data with name "{}" already exists.'.format(group_name))
+        return group_parent[group_name]
 
-	group_child.create_group('refs')
+    # ======================================
+    # Create different kinds of groups
+    # ======================================
+    group_child = group_parent.create_group(group_name)
+    group_child.create_dataset('UID', shape=(0, ), maxshape=(None, ), dtype=_np.int64)
 
-	return group_child
+    ref_dtype = h5.special_dtype(ref=h5.Reference)
+    group_child.create_dataset('dat', shape=(0, ), maxshape=(None, ), dtype=ref_dtype)
+
+    group_child.create_group('refs')
+
+    return group_child
