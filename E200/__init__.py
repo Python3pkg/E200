30b4155e4accbcb5fafc548726b11867c2f23ee4
Joel Frederico <joelfred@slac.stanford.edu>
2015-07-11 12:47:08 -0700

    Try with more files

--- E200/__init__.py
+++ E200/__init__.py
@@ -11,7 +11,7 @@ __all__ = [
     'E200_load_images',
     'classes'
     ]
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 from .setQS import *                                 # noqa
 from .E200_Image_Iter import *                       # noqa
 from .E200_load_data import *                        # noqa
